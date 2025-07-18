dfb6d08551cd97bbf511da5bb6c46ff1828de628
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-07-19 02:34:57 +0800

    no changes

--- database/migrations/0001_01_01_000007_create_puroks_table.php
+++ database/migrations/0001_01_01_000007_create_puroks_table.php
@@ -17,7 +17,7 @@ return new class extends Migration
             $table->string('purok_name');
             $table->timestamps();
         });
-    }
+    } 
 
     /**
      * Reverse the migrations.
