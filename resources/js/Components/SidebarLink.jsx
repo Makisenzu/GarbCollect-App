14c13236afd0d7d00c1459d3b4fc5ca6291b3b83
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-09-26 01:40:03 +0800

    feat('prevent drag click');

--- resources/js/Components/SidebarLink.jsx
+++ resources/js/Components/SidebarLink.jsx
@@ -8,11 +8,16 @@ export default function SidebarLink({
     as = 'a',
     ...props
 }) {
+    const handleMouseDown = (e) => {
+        e.preventDefault();
+    };
+
     return (
         <Link
             href={href}
             method={method}
             as={as}
+            onMouseDown={handleMouseDown}
             className={`flex items-center w-full px-4 py-4 transition-colors duration-200 rounded-none ${
                 active 
                     ? 'bg-green-100 text-green-800 border-l-4 border-green-600' 
