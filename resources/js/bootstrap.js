a06482cad6a81ed178727d1b797ca84d84c4b808
Denmark B Rivera <denmarkbarbarona13@gmail.com>
2025-10-04 01:24:33 +0800

    style('remove comment');

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -2,5 +2,4 @@ import axios from 'axios';
 window.axios = axios;
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
-// Import echo but don't initialize window.Echo here
 import './echo';
